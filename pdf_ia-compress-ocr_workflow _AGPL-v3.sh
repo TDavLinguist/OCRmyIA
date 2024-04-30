96db776937c9db57eda96d57321f31d3caf1c27b
Tyler D. Davis <tdavlinguist@gmail.com>
2024-04-30 22:04:50 +0700

    Update pdf_ia-compress-ocr_workflow _AGPL-v3.sh
    * Squash bug that would fail if user specified input PDF as anything outside of $pwd.
    * Remove DPI flag for now
    * Specify openjpeg as the compression algorithm
    * Settle on better image quality with the cost of slightly larger output file size

--- pdf_ia-compress-ocr_workflow _AGPL-v3.sh
+++ pdf_ia-compress-ocr_workflow _AGPL-v3.sh
@@ -3,14 +3,17 @@
 
 #The input PDF is the first argument
 INPUT_PDF=$1
-PDF_BASE="${INPUT_PDF%.*}"
+INPUT_DIR=$(dirname $INPUT_PDF)"/"
+BASENAME=$(basename $INPUT_PDF)
+PDF_BASE="${BASENAME%.*}"
 TMP_DIR=/tmp/
-DPI=300
+#DPI=300
+OCR_LANG="eng"
 
 #Perform OCR on PDF using ocrmypdf.
 LOSS_OCR_TMP="$TMP_DIR""$PDF_BASE"-loss-ocr.pdf
 echo "Now performing lossless OCR on input PDF..."
-ocrmypdf -O1 --output-type pdf $INPUT_PDF $LOSS_OCR_TMP || { echo "OCR Failed. Sorry about that."; exit 1; }
+ocrmypdf --pdf-renderer sandwich -l $OCR_LANG -O1 --output-type pdf $INPUT_PDF $LOSS_OCR_TMP || { echo "OCR Failed. Sorry about that."; exit 1; }
 echo "OCR completed successfully!"
 #Generate necessary json and hocr file from PDF
 TMP_JSON="$TMP_DIR"$PDF_BASE"_"$(date +%H%M)".json"
@@ -31,9 +34,9 @@ fi
 echo "HOCR file for $LOSS_OCR_TMP generated successfully!"
 #Run recode_pdf on the file given the proper inputs
 #First set variable for compressed PDF output.
-PDF_COMP="$PDF_BASE"-ia-ocr.pdf
+PDF_COMP="$INPUT_DIR""$PDF_BASE"-ia-ocr.pdf
 echo "Compressing OCR'd PDF with IA algorithms..."
-recode_pdf -P $LOSS_OCR_TMP -T $TMP_HOCR --dpi $DPI -o $PDF_COMP
+recode_pdf -P $LOSS_OCR_TMP -T $TMP_HOCR -J openjpeg --fg-compression-flags='-r 200' --bg-downsample 1 -m 2 -o $PDF_COMP
 # Check to see whether or not the file was successfully created.
 if [ ! -f $PDF_COMP ]; then
 	echo "Compressed PDF creation failed :("; exit 1;
